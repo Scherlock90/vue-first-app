97cae8a08245114ebfeb08971f8d753f679576ce
Sebastian Napora <sebastian.napora@intive.com>
2020-04-10 18:54:53 +0200

    add fake contact form

--- src/components/contact/Contact.vue
+++ src/components/contact/Contact.vue
@@ -1,41 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <div>
-      {{ newWord }}
-    </div>
-    <div>
-      Down below is test flex-container
-    </div>
-    <ChildComponent @sendMessageToParent="childMessageReceived" :messageTest="messTest"></ChildComponent>
-    <b-button variant="outline-primary">
-      <router-link to="/" class="router-link-custom">Go to Home</router-link>
-    </b-button>
+  <div class="contact">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+          <b-form-select
+            id="input-3"
+            v-model="form.food"
+            :options="foods"
+            required
+          ></b-form-select>
+        </b-form-group>
+
+        <b-form-group id="input-group-4">
+          <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+            <b-form-checkbox value="me">Check me out</b-form-checkbox>
+            <b-form-checkbox value="that">Check that out</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+      <!-- <b-card class="mt-3" header="Form Data Result">
+        <pre class="m-0">{{ form }}</pre>
+      </b-card> -->
   </div>
 </template>
 
 <script>
-import ChildComponent from '../ChildComponent'
-import { BButton } from 'bootstrap-vue'
+import { BButton, BCard, BForm, BFormCheckboxGroup, BFormCheckbox, BFormSelect, BFormInput } from 'bootstrap-vue'
 
 export default {
-  name: 'Contact',
   components: {
-    ChildComponent,
-    'b-button': BButton
+    BButton,
+    BCard,
+    BForm,
+    BFormCheckboxGroup,
+    BFormCheckbox,
+    BFormSelect,
+    BFormInput
   },
 
   data () {
     return {
-      msg: 'Welcome to Contact!',
-      messTest: ''
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
     }
   },
 
   methods: {
-    childMessageReceived () {
-      this.messTest = 'Create something else'
-      console.log('message from child')
+    onSubmit (evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+
+    onReset (evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
     }
   }
 }
@@ -45,6 +104,12 @@ export default {
 $white: #ffffff;
 
 //main code
+.contact {
+  height: 90%;
+  display: flex;
+  justify-content: center;
+  padding-top: 10%;
+}
 .flex-container-test{
   display: flex;
   justify-content: center;

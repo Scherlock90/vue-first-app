fa9d121cbed7f6396d0302a97c782dce7e1cf052
Sebastian Napora <sebastian.napora@intive.com>
2020-04-13 16:55:18 +0200

    add geoloection

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -4,6 +4,9 @@
     <div>
       {{ newWord }}
     </div>
+    <div>
+      <a id="map-link"></a>
+    </div>
   </div>
 </template>
 
@@ -12,14 +15,39 @@ export default {
   data () {
     return {
       msg: 'Welcome to Vue.js App!',
-      newWord: 'Something',
-      geolection: 0
+      newWord: 'Your location',
+      geolection: 0,
+      latitude: 0,
+      longitude: 0,
+      status: ''
     }
   },
   methods: {
     geolocation () {
-      console.log(navigator.geolocation.getCurrentPosition())
-      this.geolection = navigator.geolocation.getCurrentPosition()
+      if (navigator.geolocation) {
+        (function () {
+          const mapLink = document.querySelector('#map-link')
+
+          function success (position) {
+            const lat = position.coords.latitude
+            const lang = position.coords.longitude
+
+            mapLink.href = `https://www.openstreetmap.org/#map=18/${lat}/${lang}`
+            mapLink.textContent = `Latitude: ${lat} °, Longitude: ${lang} °`
+          }
+
+          function error () {
+            console.log('Unable to retrieve your location')
+          }
+
+          if (!navigator.geolocation) {
+            console.log('Geolocation is not supported by your browser')
+          } else {
+            console.log('Locating…')
+            navigator.geolocation.getCurrentPosition(success, error)
+          }
+        })()
+      }
     }
   },
   mounted () {

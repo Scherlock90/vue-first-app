98fe92d4754af128a04c28e451764feb79f6aa22
Sebastian Napora <sebastian.napora@intive.com>
2020-04-27 09:31:53 +0200

    extend Cards test

--- test/unit/specs/Cards.spec.js
+++ test/unit/specs/Cards.spec.js
@@ -7,8 +7,8 @@ function getMountedComponent (Component, propsData) {
   })
 }
 
-describe('MyComponent', () => {
-  it('renders correctly with props', () => {
+describe('Common Cards', () => {
+  it('renders correctly with all props and diffrent', () => {
     expect(
       getMountedComponent(Cards, {
         id: 'card1',
@@ -25,6 +25,52 @@ describe('MyComponent', () => {
       title: 'something-about-me',
       description: 'something',
       redirect: 'goToGithub'
+    })
+
+    expect(
+      getMountedComponent(Cards, {
+        id: 'card3',
+        pathToImage: 'mark.jpg',
+        nameImage: 'your-face',
+        title: 'something-about-you',
+        description: 'something?',
+      }).props()
+    ).toEqual({
+      id: 'card3',
+      pathToImage: 'mark.jpg',
+      nameImage: 'your-face',
+      title: 'something-about-you',
+      description: 'something?',
+    })
+  })
+
+  it('renders correctly without not required props', () => {
+    expect(
+      getMountedComponent(Cards, {
+        pathToImage: 'sen.jpg',
+        nameImage: 'my-face',
+        title: 'something-about-me',
+        description: 'something',
+      }).props()
+    ).toEqual({
+      pathToImage: 'sen.jpg',
+      nameImage: 'my-face',
+      title: 'something-about-me',
+      description: 'something',
+    })
+
+    expect(
+      getMountedComponent(Cards, {
+        pathToImage: 'mark.jpg',
+        nameImage: 'your-face',
+        title: 'something-about-you',
+        description: 'something?',
+      }).props()
+    ).toEqual({
+      pathToImage: 'mark.jpg',
+      nameImage: 'your-face',
+      title: 'something-about-you',
+      description: 'something?',
     })
   })
 })
